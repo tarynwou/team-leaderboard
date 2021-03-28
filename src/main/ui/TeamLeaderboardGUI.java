62a6ff97f4cc2e7c07d8ee33bd5a2bc09af01c1d
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2021-03-28 01:43:12 -0700

    Added SpringUtilities but the Entry panel is not showing its fields

--- src/main/ui/TeamLeaderboardGUI.java
+++ src/main/ui/TeamLeaderboardGUI.java
@@ -11,6 +11,8 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,7 +22,7 @@ import java.util.ConcurrentModificationException;
 // CREDITS: LabelChanger and TextInputDemo
 
 // Creates the graphical user interfaces for Team Leaderboard
-public class TeamLeaderboardGUI extends JFrame implements ActionListener {
+public class TeamLeaderboardGUI extends JFrame implements ActionListener, FocusListener {
     // Save and reload fields
     private static final String JSON_STORE = "./data/leaderboard.json";
     private JsonWriter jsonWriter = new JsonWriter(JSON_STORE);
@@ -36,8 +38,8 @@ public class TeamLeaderboardGUI extends JFrame implements ActionListener {
     // Fields
     JTextField nameField = new JTextField();
     JTextField commentField = new JTextField();
-    JTextField teammateField = new JTextField();
-    JSpinner actionField;
+    JTextField teammateField;
+    JSpinner actionSpinner;
 
     // Labels
     JLabel nameLabel;
@@ -67,8 +69,8 @@ public class TeamLeaderboardGUI extends JFrame implements ActionListener {
         // You could also set a different object, if you wanted
         // a different object to respond to the button click
         label = new JLabel("flag");
-        field = new JTextField(5);
-        add(field);
+        teammateField = new JTextField(5);
+        add(teammateField);
         add(btn);
         add(label);
         pack();
@@ -82,18 +84,18 @@ public class TeamLeaderboardGUI extends JFrame implements ActionListener {
         JPanel middlePanel = new JPanel();
         JPanel rightPanel = new JPanel();
 
-        leftPanel.setBounds(0, 0, SCREEN_WIDTH / 4, SCREEN_HEIGHT);
-        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
+        leftPanel.setBounds(0, 0, SCREEN_WIDTH / 8, SCREEN_HEIGHT);
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
         leftPanel.add(createLeftButtons());
-        leftPanel.setBackground(Color.BLUE);
+//        leftPanel.setBackground(Color.BLUE);
 
-        middlePanel.setBounds(SCREEN_WIDTH / 4, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT);
+        middlePanel.setBounds(SCREEN_WIDTH / 8, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT);
         middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.PAGE_AXIS));
         middlePanel.setBackground(Color.CYAN);
         middlePanel.add(setUpMiddlePanel());
 
-        rightPanel.setBounds(SCREEN_WIDTH * 3 / 4, 0, SCREEN_WIDTH / 4, SCREEN_HEIGHT);
-        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
+        rightPanel.setBounds(SCREEN_WIDTH * 5 / 8, 0, SCREEN_WIDTH * 3 / 8, SCREEN_HEIGHT);
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
         rightPanel.add(setUpRightPanel());
         rightPanel.setBackground(Color.GREEN);
 
@@ -136,14 +138,27 @@ public class TeamLeaderboardGUI extends JFrame implements ActionListener {
         JPanel rightPanelSetup = new JPanel();
         JPanel teammatePanel = new JPanel();
         JPanel entryPanel = new JPanel();
-        JLabel teammatesTitle = new JLabel("TEAMMATES");
-        JLabel entryTitle = new JLabel("LOG ENTRY");
 
-        rightPanelSetup.setBounds(0, 0, SCREEN_WIDTH / 4, SCREEN_HEIGHT);
         rightPanelSetup.add(teammatePanel);
         rightPanelSetup.add(entryPanel);
         rightPanelSetup.setBackground(Color.PINK);
 
+        setUpTeammatePanel(teammatePanel);
+        setUpEntryPanel(entryPanel);
+
+        return rightPanelSetup;
+    }
+
+    private void setUpEntryPanel(JPanel entryPanel) {
+        JLabel entryTitle = new JLabel("LOG ENTRY");
+        entryPanel.setLocation(0, SCREEN_HEIGHT / 2);
+        entryPanel.setBackground(Color.RED);
+        entryPanel.add(entryTitle);
+        createEntryFields();
+    }
+
+    private void setUpTeammatePanel(JPanel teammatePanel) {
+        JLabel teammatesTitle = new JLabel("TEAMMATES");
         teammatePanel.setLayout(new BoxLayout(teammatePanel, BoxLayout.Y_AXIS));
         //TODO: setBounds doesn't work
         teammatePanel.setBounds(0, 0, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2);
@@ -152,24 +167,6 @@ public class TeamLeaderboardGUI extends JFrame implements ActionListener {
         addTextField(teammatePanel, nameLabel, nameField, "Name: ");
         addButton(teammatePanel, "Add", "add");
         addButton(teammatePanel, "Remove", "remove");
-
-//        entryPanel.setLayout(new SpringLayout(teammatePanel, SpringLayout.HEIGHT));
-//        entryPanel.setLayout(new SpringLayout());
-//        entryPanel.makeCompactGrid();
-        //TODO: setBounds doesn't work
-        entryPanel.setBounds(0, SCREEN_HEIGHT / 2, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2);
-        entryPanel.setBackground(Color.RED);
-        entryPanel.add(entryTitle);
-        actionLabel = new JLabel("Action: ");
-        actionField = new JSpinner();
-        entryPanel.add(actionLabel);
-        entryPanel.add(actionField);
-
-
-        addTextField(entryPanel, commentLabel, commentField, "Comment: ");
-        addTextField(entryPanel, teammateLabel, teammateField, "Teammate: ");
-
-        return rightPanelSetup;
     }
 
     public void addTextField(JPanel panel, JLabel label, JTextField field, String text) {
@@ -177,6 +174,87 @@ public class TeamLeaderboardGUI extends JFrame implements ActionListener {
         field.setColumns(10);
         panel.add(label);
         panel.add(field);
+    }
+
+    protected JComponent createEntryFields() {
+        JPanel panel = new JPanel(new SpringLayout());
+
+        String[] labelStrings = {
+                "Action: ",
+                "Comment: ",
+                "Teammate: "
+        };
+
+        JLabel[] labels = new JLabel[labelStrings.length];
+        JComponent[] fields = new JComponent[labelStrings.length];
+        int fieldNum = 0;
+
+        initializeEntryFields(fields, fieldNum);
+        pairLabelsAndFields(panel, labelStrings, labels, fields);
+        SpringUtilities.makeCompactGrid(panel,
+                labelStrings.length, 2,
+                GAP, GAP, //init x,y
+                GAP, GAP / 2);//xpad, ypad
+        return panel;
+    }
+
+    private void pairLabelsAndFields(JPanel panel, String[] labelStrings, JLabel[] labels, JComponent[] fields) {
+        // Associate label/field pairs, add everything, and lay it out.
+        for (int i = 0; i < labelStrings.length; i++) {
+            labels[i] = new JLabel(labelStrings[i],
+                    JLabel.TRAILING);
+            labels[i].setLabelFor(fields[i]);
+            panel.add(labels[i]);
+            panel.add(fields[i]);
+
+            //Add listeners to each field.
+            JTextField tf = null;
+            if (fields[i] instanceof JSpinner) {
+                tf = getTextField((JSpinner) fields[i]);
+            } else {
+                tf = (JTextField) fields[i];
+            }
+            tf.addActionListener(this);
+            tf.addFocusListener(this);
+        }
+    }
+
+    private void initializeEntryFields(JComponent[] fields, int fieldNum) {
+        //Create the text field and set it up.
+        String[] actions = getActions();
+        actionSpinner = new JSpinner(new SpinnerListModel(actions));
+        fields[fieldNum++] = actionSpinner;
+
+        commentField  = new JTextField();
+        commentField.setColumns(10);
+        fields[fieldNum++] = commentField;
+
+        teammateField = new JTextField();
+        teammateField.setColumns(10);
+        fields[fieldNum++] = teammateField;
+    }
+
+    private String[] getActions() {
+        String[] stateStrings = {
+                "Copywriting (150pts)",
+                "Marketing (100pts)",
+                "Research (100pts)",
+                "something good (50pts)" // TODO: Change to "Bonus"
+        };
+        return stateStrings;
+    }
+
+    //TODO: review this
+    public JFormattedTextField getTextField(JSpinner spinner) {
+        JComponent editor = spinner.getEditor();
+        if (editor instanceof JSpinner.DefaultEditor) {
+            return ((JSpinner.DefaultEditor)editor).getTextField();
+        } else {
+            System.err.println("Unexpected editor type: "
+                    + spinner.getEditor().getClass()
+                    + " isn't a descendant of DefaultEditor");
+            return null;
+        }
     }
 
     protected JComponent createLeftButtons() {
@@ -210,7 +288,7 @@ public class TeamLeaderboardGUI extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("myButton")) {
-            label.setText(field.getText());
+            label.setText(teammateField.getText());
         } else if (e.getActionCommand().equals("quit")) {
             System.exit(0);
         } else if (e.getActionCommand().equals("add")) {
@@ -271,5 +349,31 @@ public class TeamLeaderboardGUI extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
         new TeamLeaderboardGUI();
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        Component c = e.getComponent();
+        if (c instanceof JFormattedTextField) {
+            selectItLater(c);
+        } else if (c instanceof JTextField) {
+            ((JTextField)c).selectAll();
+        }
+    }
+
+    private void selectItLater(Component c) {
+        if (c instanceof JFormattedTextField) {
+            final JFormattedTextField ftf = (JFormattedTextField)c;
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    ftf.selectAll();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        // do nothing
     }
 }
